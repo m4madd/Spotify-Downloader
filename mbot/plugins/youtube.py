bd685266c3b15c3d82b03096751c1318e290bb4b
m4madd <83618301+m4madd@users.noreply.github.com>
2022-07-24 10:26:59 -0700

    Update youtube.py

--- mbot/plugins/youtube.py
+++ mbot/plugins/youtube.py
@@ -29,7 +29,7 @@ from mbot.utils.ytdl import getIds,ytdl_down,audio_opt,thumb_down
 
 @Mbot.on_message(filters.regex(r'(https?://)?.*you[^\s]+') & filters.private | filters.command(["yt","ytd","ytmusic"]) & filters.regex(r'https?://.*you[^\s]+') & filters.chat(AUTH_CHATS))
 async def _(_,message):
-    m = await message.reply_text("Gathering information... Please Wait.")
+    m = await message.reply_text("در حال جمع آوری اطلاعات... لطفا صبر کنید.")
     link = message.matches[0].group(0)
     if link in [
         "https://youtube.com/",
@@ -46,7 +46,7 @@ async def _(_,message):
         randomdir = "/tmp/"+str(randint(1,100000000))
         mkdir(randomdir)
         for id in ids:
-            PForCopy = await message.reply_photo(f"https://i.ytimg.com/vi/{id[0]}/hqdefault.jpg",caption=f"🎧 Title : `{id[3]}`\n🎤 Artist : `{id[2]}`\n💽 Track No : `{id[1]}`\n💽 Total Track : `{videoInPlaylist}`")
+            PForCopy = await message.reply_photo(f"https://i.ytimg.com/vi/{id[0]}/hqdefault.jpg",caption=f"🎧 عنوان : `{id[3]}`\n🎤 هنرمند : `{id[2]}`\n💽 شماره آهنگ : `{id[1]}`\n💽 تعداد آهنگ : `{videoInPlaylist}`")
             fileLink = await ytdl_down(audio_opt(randomdir,id[2]),id[0])
             thumnail = await thumb_down(id[0])
             AForCopy = await message.reply_audio(fileLink,caption=f"[{id[3]}](https://youtu.be/{id[0]}) - {id[2]}",title=id[3].replace("_"," "),performer=id[2],thumb=thumnail,duration=id[4])
